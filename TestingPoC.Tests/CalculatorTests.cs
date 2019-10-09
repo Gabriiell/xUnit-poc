9541c2e3ddcea92776c8c1f1f1e00d9439a3bfab
Gabriel O Pereyra <Gabriel.O.Pereyra@ar.ey.com>
2019-10-09 16:55:02 -0300

    Add xunit reference

---
+++ TestingPoC.Tests/CalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using TestingPoc;
+using Xunit;
+
+namespace TestingPoC.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void ShouldAdd()
+        {
+            var sut = new Calculator();
+            var result = sut.Add(1, 2);
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ TestingPoc/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingPoc
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
