45de29d65739bcb198be6ddf5200c48f8bfecaad
Gabriiell <gabriel.opereyra@gmail.com>
2019-10-12 23:37:17 -0300

    Add null and boolean tests

---
+++ TestingPoC.Tests/NullAndBoolTests.cs
@@ -0,0 +1,29 @@
+﻿using TestingPoc;
+using Xunit;
+
+namespace TestingPoC.Tests
+{
+    public class NullAndBoolTests
+    {
+        [Fact]
+        public void ShouldHaveAGeneratedName()
+        {
+            var sut = new PlayerCharacter();
+            Assert.False(string.IsNullOrWhiteSpace(sut.Name));
+        }
+
+        [Fact]
+        public void ShouldNotHaveANickName()
+        {
+            var sut = new PlayerCharacter();
+            Assert.Null(sut.NickName);
+        }
+
+        [Fact]
+        public void ShouldBeNewbie()
+        {
+            var sut = new PlayerCharacter();
+            Assert.True(sut.IsNoob);
+        }
+    }
+}
---
+++ TestingPoc/PlayerCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingPoc
+{
+    public class PlayerCharacter
+    {
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        public bool IsNoob { get; set; }
+        public int Health { get; set; }
+        public List<string> Weapons { get; set; }
+
+        public PlayerCharacter()
+        {
+            IsNoob = true;
+            Name = GenerateName();
+        }
+
+        private string GenerateName()
+        {
+            var names = new[]
+            {
+                "Danieth",
+                "Derick",
+                "Shalnorr",
+                "Toth",
+                "Boldrak"
+            };
+
+            return names[new Random().Next(0, names.Length)];
+        }
+    }
+}
