0a7fe50574fa6efdac69c8ea606d30574259a537
Gabriiell <gabriel.opereyra@gmail.com>
2019-10-16 23:57:00 -0300

    Add test outputs

--- TestingPoC.Tests/RangeTests.cs
+++ TestingPoC.Tests/RangeTests.cs
@@ -1,15 +1,28 @@
 ﻿using TestingPoc;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace TestingPoC.Tests
 {
     public class RangeTests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public RangeTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void ShouldIncreaseHealthAfterSleeping()
         {
+            _testOutputHelper.WriteLine("Creating Player");
             var sut = new PlayerCharacter { Health = 100 };
+
+            _testOutputHelper.WriteLine("Player going to sleep");
             sut.Sleep();
+            _testOutputHelper.WriteLine("Player awoken");
+
             Assert.InRange(sut.Health, 101, 200);
         }
     }
