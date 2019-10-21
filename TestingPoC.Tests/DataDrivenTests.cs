b6c1044bf40ba9203f5460fbd9cf6ff3652f1e20
Gabriiell <gabriel.opereyra@gmail.com>
2019-10-21 00:34:42 -0300

    Add data driven test

---
+++ TestingPoC.Tests/DataDrivenTests.cs
@@ -0,0 +1,19 @@
+﻿using TestingPoc;
+using Xunit;
+
+namespace TestingPoC.Tests
+{
+    public class DataDrivenTests
+    {
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, 0, 1)]
+        [InlineData(1, -4, -3)]
+        public void ShouldAddIntValues(int first, int second, int expectedResult)
+        {
+            var sut = new Calculator();
+            var result = sut.Add(first, second);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
