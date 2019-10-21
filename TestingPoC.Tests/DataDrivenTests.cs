36e5e7d57df88557a69a9feff8351fca1239b5c9
Gabriiell <gabriel.opereyra@gmail.com>
2019-10-21 00:49:56 -0300

    Refactor data driven test with class data

--- TestingPoC.Tests/DataDrivenTests.cs
+++ TestingPoC.Tests/DataDrivenTests.cs
@@ -1,8 +1,22 @@
-﻿using TestingPoc;
+﻿using System.Collections.Generic;
+using TestingPoc;
 using Xunit;
 
 namespace TestingPoC.Tests
 {
+    public class CalculatorTestData
+    {
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                yield return new object[] { 1, 1, 0 };
+                yield return new object[] { 15, 6, 9 };
+                yield return new object[] { 10, 11, -1 };
+            }
+        }
+    }
+
     public class DataDrivenTests
     {
         [Theory]
@@ -13,6 +27,15 @@ namespace TestingPoC.Tests
         {
             var sut = new Calculator();
             var result = sut.Add(first, second);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(CalculatorTestData.TestData), MemberType = typeof(CalculatorTestData))]
+        public void ShouldSubstractIntValues(int first, int second, int expectedResult)
+        {
+            var sut = new Calculator();
+            var result = sut.Substract(first, second);
             Assert.Equal(expectedResult, result);
         }
     }
--- TestingPoc/Calculator.cs
+++ TestingPoc/Calculator.cs
@@ -11,6 +11,11 @@ namespace TestingPoc
             return a + b;
         }
 
+        public int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
         public double AddDoubles(double a, double b)
         {
             return a + b;
